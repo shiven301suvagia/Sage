b0acd5e69a7774efde545bd2a1c4aafe874e51d1
Shiven suvagia <shivensuvagia301@gmail.com>
2026-08-31 14:45:41 +0530

    ES-007: Export tool system

---
+++ frontend/src/tools/index.ts
@@ -0,0 +1,4 @@
+export type { ToolContext, ToolDefinition, ToolResult, ToolRisk } from './ToolContracts.js';
+export { ToolRegistry } from './ToolRegistry.js';
+export { ToolRouter } from './ToolRouter.js';
+export { createEchoTool, createGetTimeTool } from './BuiltinTools.js';
