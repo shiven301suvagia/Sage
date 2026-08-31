b34173f050eedb43ee25de8fad22a1f8d4fcdcd2
Shiven suvagia <shivensuvagia301@gmail.com>
2026-08-31 14:49:57 +0530

    ES-008: Export system integration

---
+++ frontend/src/system/index.ts
@@ -0,0 +1,3 @@
+export { RuntimeSystemContext } from './SystemContext.js';
+export type { SystemSnapshot, SystemContextProvider } from './SystemContext.js';
+export { createSystemContextTool } from './SystemContextTool.js';
