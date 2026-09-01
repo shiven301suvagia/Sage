afe3f8c75fac83fd165811f22bc70c959eadbad0
Shiven suvagia <shivensuvagia301@gmail.com>
2026-09-01 18:55:16 +0530

    rebuild: simplify secure desktop preload API

--- app/preload.mjs
+++ app/preload.mjs
@@ -1,17 +1,9 @@
 import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('sage', Object.freeze({
-  window: Object.freeze({
-    resize: (mode) => ipcRenderer.invoke('window:resize', mode),
-    moveBy: (dx, dy) => ipcRenderer.invoke('window:move-by', dx, dy),
-    hide: () => ipcRenderer.invoke('window:hide'),
-    close: () => ipcRenderer.invoke('window:close'),
-    center: () => ipcRenderer.invoke('window:center'),
-    alwaysOnTop: () => ipcRenderer.invoke('window:always-on-top')
-  }),
-  network: Object.freeze({ get: () => ipcRenderer.invoke('network:get'), set: (value) => ipcRenderer.invoke('network:set', value) }),
-  ask: (text) => ipcRenderer.invoke('assistant:message', text),
-  memory: Object.freeze({ list: () => ipcRenderer.invoke('memory:list'), clear: () => ipcRenderer.invoke('memory:clear') }),
-  reminders: Object.freeze({ list: () => ipcRenderer.invoke('reminder:list') }),
-  onReminder: (callback) => { const listener = (_event, item) => callback(item); ipcRenderer.on('reminder:fired', listener); return () => ipcRenderer.removeListener('reminder:fired', listener); }
+contextBridge.exposeInMainWorld('sage',Object.freeze({
+ window:Object.freeze({resize:m=>ipcRenderer.invoke('window:resize',m),moveBy:(x,y)=>ipcRenderer.invoke('window:move-by',x,y),hide:()=>ipcRenderer.invoke('window:hide'),close:()=>ipcRenderer.invoke('window:close'),center:()=>ipcRenderer.invoke('window:center'),alwaysOnTop:()=>ipcRenderer.invoke('window:always-on-top')}),
+ network:Object.freeze({get:()=>ipcRenderer.invoke('network:get'),set:v=>ipcRenderer.invoke('network:set',v)}),
+ ask:t=>ipcRenderer.invoke('assistant:message',t),
+ memory:Object.freeze({list:()=>ipcRenderer.invoke('memory:list'),clear:()=>ipcRenderer.invoke('memory:clear')}),
+ reminders:Object.freeze({list:()=>ipcRenderer.invoke('reminder:list')}),
+ onReminder:cb=>{const f=(_e,v)=>cb(v);ipcRenderer.on('reminder:fired',f);return()=>ipcRenderer.removeListener('reminder:fired',f)}
 }));
