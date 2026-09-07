be8893f8b0e60e0fe8cf9968dd78417f379cac1d
Shiven suvagia <shivensuvagia301@gmail.com>
2026-09-07 22:07:21 +0530

    feat: expose voice permission boundary

--- app/preload.mjs
+++ app/preload.mjs
@@ -5,6 +5,7 @@ contextBridge.exposeInMainWorld('sage',Object.freeze({
  memory:Object.freeze({list:()=>ipcRenderer.invoke('memory:list'),search:q=>ipcRenderer.invoke('memory:search',String(q)),clear:()=>ipcRenderer.invoke('memory:clear')}),
  reminder:Object.freeze({list:()=>ipcRenderer.invoke('reminder:list'),onDue:fn=>ipcRenderer.on('reminder:due',(_e,data)=>fn(data))}),
  network:Object.freeze({get:()=>ipcRenderer.invoke('network:get'),set:v=>ipcRenderer.invoke('network:set',Boolean(v))}),
+ voice:Object.freeze({canUse:()=>ipcRenderer.invoke('voice:can-use')}),
  experience:Object.freeze({get:()=>ipcRenderer.invoke('experience:get')}),
  proactive:Object.freeze({get:()=>ipcRenderer.invoke('proactive:get'),set:v=>ipcRenderer.invoke('proactive:set',Boolean(v)),onSuggestion:fn=>ipcRenderer.on('proactive:suggestion',(_e,data)=>fn(data))})
 }));
