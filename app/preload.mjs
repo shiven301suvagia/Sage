82c38e34d8c754419e9b81342bbdf033a9cb3b56
Shiven suvagia <shivensuvagia301@gmail.com>
2026-09-08 15:10:39 +0530

    feat: add privacy-safe desktop context layer
    * feat: add privacy-safe desktop context layer
    
    * feat: add safe Windows foreground app context
    
    * feat: expose active desktop app in context
    
    * feat: register desktop context tools
    
    * feat: integrate live Windows desktop context

--- app/preload.mjs
+++ app/preload.mjs
@@ -8,5 +8,6 @@ contextBridge.exposeInMainWorld('sage',Object.freeze({
  voice:Object.freeze({canUse:()=>ipcRenderer.invoke('voice:can-use'),getPreference:()=>ipcRenderer.invoke('voice:get-preference'),setPreference:v=>ipcRenderer.invoke('voice:set-preference',Boolean(v))}),
  experience:Object.freeze({get:()=>ipcRenderer.invoke('experience:get')}),
  proactive:Object.freeze({get:()=>ipcRenderer.invoke('proactive:get'),set:v=>ipcRenderer.invoke('proactive:set',Boolean(v)),onSuggestion:fn=>ipcRenderer.on('proactive:suggestion',(_e,data)=>fn(data))}),
- plugins:Object.freeze({list:()=>ipcRenderer.invoke('plugin:list')})
+ plugins:Object.freeze({list:()=>ipcRenderer.invoke('plugin:list')}),
+ context:Object.freeze({get:()=>ipcRenderer.invoke('context:get')})
 }));
